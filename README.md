b95168a2b5144d599dfa9232ae945ce76a540c69
Hannah Read <hannahread@Alethiea.local>
2021-12-14 13:51:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A practice application for built in python types implementing a simple command line todo list app.
+Includes doctests.
---
+++ tasker.py
@@ -0,0 +1,11 @@
+def print_todo(todo):
+    """
+    Accepts a dict and prints it with a format string
+
+    >>> todo = {'name': 'Example Name', 'body': 'Example Body', 'points': '1'}
+    >>> print_todo(todo)
+    Example Name: Example Body
+    >>>
+    """
+    pass
+    print(f"{todo['name']}: {todo['body']}")
